84e1bad8040057e1c711c7f9d71c41a5d27f3d9a
Talal Al Ajmi <talalajmi98@gmail.com>
2023-06-10 20:17:54 +0400

    Latest Changes

---
+++ app/liked/components/LikedSongs.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { Song } from "@/types";
+import { useUser } from "@/hooks/useUser";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+// import useOnPlay from "@/hooks/useOnPlay";
+
+interface LikedSongsProps {
+  songs: Song[];
+}
+
+const LikedSongs: React.FC<LikedSongsProps> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  //   const onPlay = useOnPlay(songs);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
+  if (songs.length === 0) {
+    return (
+      <div
+        className="
+          flex 
+          w-full 
+          flex-col 
+          gap-y-2 px-6 
+          text-neutral-400
+        "
+      >
+        No liked songs.
+      </div>
+    );
+  }
+  return (
+    <div className="flex w-full flex-col gap-y-2 p-6">
+      {songs.map((song: Song) => (
+        <div key={song.id} className="flex w-full items-center gap-x-4">
+          <div className="flex-1">
+            <MediaItem onClick={() => {}} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LikedSongs;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,7 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
+import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
 
 interface SearchProps {
   searchParams: {
@@ -8,7 +11,17 @@ interface SearchProps {
 
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
-  return <div>Search</div>;
+  return (
+    <div className="h-full w-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
+      <Header className="from-bg-black">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-3xl font-semibold text-white">Search</h1>
+        </div>
+        <SearchInput />
+      </Header>
+      <SearchContent songs={songs} />
+    </div>
+  );
 };
 
 export default Search;
